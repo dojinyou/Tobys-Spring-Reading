efd1f2996f58630223da6d526dee929fe88d14f7
dojinyou <djyou128@gmail.com>
2022-09-02 16:58:48 +0900

    :recycle: ApplicationContext 적용

--- ObjectAndDependency/src/main/java/com/dojinyou/dao/DaoFactory.java
+++ ObjectAndDependency/src/main/java/com/dojinyou/dao/DaoFactory.java
@@ -1,6 +1,11 @@
 package com.dojinyou.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+  @Bean
   public UserDao userDao() {
     return new UserDao(getConnectionMaker());
   }
@@ -14,6 +19,7 @@ public class DaoFactory {
     return new MessageDao(getConnectionMaker());
   }
 
+  @Bean
   private static SimpleConnectionMaker getConnectionMaker() {
     return new SimpleConnectionMaker();
   }
--- ObjectAndDependency/src/test/java/com/dojinyou/dao/UserDaoTest.java
+++ ObjectAndDependency/src/test/java/com/dojinyou/dao/UserDaoTest.java
@@ -2,9 +2,14 @@ package com.dojinyou.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 class UserDaoTest {
   public static void main(String[] args) throws ClassNotFoundException, SQLException {
-    UserDao dao = new DaoFactory().userDao();
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+    UserDao dao = context.getBean("userDao", UserDao.class);
 
     User user = new User();
     user.setId("whiteship");
