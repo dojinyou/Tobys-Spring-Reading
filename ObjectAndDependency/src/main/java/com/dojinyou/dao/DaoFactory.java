1369da1c915cd1716d5e11f93fe0336ab2cfa6c3
dojinyou <djyou128@gmail.com>
2022-09-02 12:52:30 +0900

    :recycle: 메소드 추출

--- ObjectAndDependency/src/main/java/com/dojinyou/dao/DaoFactory.java
+++ ObjectAndDependency/src/main/java/com/dojinyou/dao/DaoFactory.java
@@ -2,14 +2,20 @@ package com.dojinyou.dao;
 
 public class DaoFactory {
   public UserDao userDao() {
-    return new UserDao(new SimpleConnectionMaker());
+    return new UserDao(getConnectionMaker());
   }
 
+
   public AccountDao accountDao() {
-    return new AccountDao(new SimpleConnectionMaker());
+    return new AccountDao(getConnectionMaker());
   }
 
   public MessageDao messageDao() {
-    return new MessageDao(new SimpleConnectionMaker());
+    return new MessageDao(getConnectionMaker());
   }
+
+  private static SimpleConnectionMaker getConnectionMaker() {
+    return new SimpleConnectionMaker();
+  }
+
 }
